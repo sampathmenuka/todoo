4297eed3e3f628b9b91630e6a2883e7431e8b583
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-10-28 18:01:41 +0530

    feat: add dotenv package with support for .env file parsing and encryption
    - Introduced dotenv package to load environment variables from .env files.
    - Implemented support for encrypted .env.vault files using DOTENV_KEY.
    - Added CLI options parsing for dotenv configuration.
    - Created TypeScript definitions for better type safety.
    - Included security guidelines for reporting vulnerabilities.
    - Enhanced documentation and examples for usage.

--- TODO/index.js
+++ TODO/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 
+const PORT = process.env.SERVER_PORT || 3000;
+ 
 app.listen(3000,()=>{
-    console.log('server started and running on port 3000');
+    console.log(' server started and running on port 3000');
 })
+
+app.get('/test',(req,resp)=>{
+    return resp.json({'massage':'server started'})
+});
