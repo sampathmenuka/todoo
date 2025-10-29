09478268f146d76c3684192b736032c07cb12d90
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-10-29 12:06:44 +0530

    fix: correct response methods in signup and login functions

--- TODO/controller/UserController.js
+++ TODO/controller/UserController.js
@@ -9,22 +9,23 @@ const signup = async(req, resp)=>{
     try{
         const {name,email,password} = req.body;
         const existingUser = await User.findOne({email});
-        if(existingUser) return resp.state(409).json({'massage':"User alredy exists"});
+        if(existingUser) return resp.status(409).json({'massage':"User alredy exists"});
 
         const hashedPassword = await bcrypt.hash(password,10);
         const savedUser = await User.create({
             name,email,passwordHash:hashedPassword
         });
-        resp.state(201).json({'massage':"User created Succesfully",userId:savedUser._id});
+        resp.status(201).json({'massage':"User created Succesfully",userId:savedUser._id});
     }catch (e){
-        resp.state(500).json({'massage':'Signup error',error:e});
+        resp.status(500).json({'massage':'Signup error',error:e});
     }
 }
 
 const login = async(req, resp)=>{
     try{
+        console.log(req.body);
         const {email, password}= req.body;
-        const selectUser= await User.findOne{email};
+        const selectUser= await User.findOne({email});
         if(!selectUser) return resp.status(404).json({'massage':"User not found"});
 
         const isPasswordValid = await bcrypt .compare(password, selectUser.passwordHash);
@@ -33,7 +34,7 @@ const login = async(req, resp)=>{
         const token = jwt.sign({userId:selectUser._id, email:selectUser.email}, JWT_SECRET,{expiresIn:'1h'});
         resp.status(200).json({'massage':'success',token:token})
 }catch (e){
-    resp.state(500).json({'massage':'login error',error:e});
+    resp.status(500).json({'massage':'login error',error:e});
     }
     
 }
