a9b2d6551c63a03f33fc15192d7ec1c9c206cb3b
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-11-01 18:53:38 +0530

    feat: add initial frontend structure with HTML, CSS, and JavaScript for user authentication and dashboard

---
+++ TODO/frontend/scripts/main.js
@@ -0,0 +1,11 @@
+function getCookie(name){
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if(parts.length===2){
+        return
+    }
+}
+
+document.getElementById('btncreate').addEventListener("click", function() {
+    
+});
