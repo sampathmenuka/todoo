5df89cc379cdde70b9f426d344c796e20c8e63ae
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-11-18 20:50:50 +0530

    feat: implement localStorage for token management and redirect to dashboard on login/signup

--- TODO/frontend/scripts/main.js
+++ TODO/frontend/scripts/main.js
@@ -1,18 +1,18 @@
-function getCookie(name){
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if(parts.length===2){
-        return parts.pop().split(';').shift();
-    }
-}
+// function getCookie(name){
+//     const value = `; ${document.cookie}`;
+//     const parts = value.split(`; ${name}=`);
+//     if(parts.length===2){
+//         return parts.pop().split(';').shift();
+//     }
+// }
 
 document.getElementById('btncreate')
 .addEventListener("click", function() {
-    const token = getCookie('token');
+    const token = localStorage.getItem('token');
     if(token){
-        window.location.href='dashboard.html';
+        window.location.href='pages/dashboard.html';
     }else{
-        window.location.href='login.html';
+        window.location.href='pages/login.html';
     }
     
 });
